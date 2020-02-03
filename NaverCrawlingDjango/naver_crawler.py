9bcced42a5399833a65cf09bc8b8c0dc1ae88aab
gorani <gorani4545@gmail.com>
2020-02-03 17:30:18 +0900

    2020-02-03 Naver 코로나바이러스 검색어 크롤링

--- NaverCrawlingDjango/naver_crawler.py
+++ NaverCrawlingDjango/naver_crawler.py
@@ -7,21 +7,21 @@ from bs4 import BeautifulSoup
 def fetch_naver_latest_data():
     result = []
 
-    url = 'https://search.naver.com/search.naver?where=news&sm=tab_jum&query=%EC%9A%B0%ED%95%9C'
+    url = 'https://search.naver.com/search.naver?where=news&sm=tab_jum&query=%EC%BD%94%EB%A1%9C%EB%82%98%EB%B0%94%EC%9D%B4%EB%9F%AC%EC%8A%A4'
     response = requests.get(url)
     html = response.text
     soup = BeautifulSoup(html, 'html.parser')
 
     web_page_link_root = "https://search.naver.com"
-    list_items = soup.find_all("li")
-    print(list_items)
+    list_items = soup.find_all("a", "_sp_each_title")
+
     for item in list_items:
         # title
-        title = item.find("a", "_sp_each_title")
-        print(title)
+        title = item.text
 
         # link
-        page_link_raw = web_page_link_root + item.find("href")
+        link = item["href"]
+        page_link_raw = web_page_link_root + item["href"]
         page_link_parts = urlparse(page_link_raw)
         normalized_page_link = page_link_parts.scheme + '://' + page_link_parts.hostname + page_link_parts.path
 
@@ -38,9 +38,6 @@ def fetch_naver_latest_data():
         result.append(item_obj)
 
     return result
-
-if __name__ == '__main__':
-    fetch_naver_latest_data()
 
 
 
